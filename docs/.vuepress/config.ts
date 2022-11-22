be9d9964c72b2e6169efa9b0d5c7da2ed195d0d0
QianJunJie <1637686265@qq.com>
2022-11-22 16:55:36 +0800

    修改1

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,6 +4,7 @@
 import { resolve } from 'path'
 import { defineConfig4CustomTheme, UserPlugins } from 'vuepress/config'
 import { VdoingThemeConfig } from 'vuepress-theme-vdoing/types'
+// @ts-ignore
 import dayjs from 'dayjs'
 import baiduCode from './config/baiduCode' // 百度统计hm码
 import htmlModules from './config/htmlModules' // 自定义插入的html块
@@ -63,6 +64,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
               {
                 text: 'JS设计模式总结',
                 link: '/pages/4643cd/',
+              },
+              {
+                text: '个人学习笔记',
+                link: '/pages/7f1ff0/',
               },
             ],
           },
