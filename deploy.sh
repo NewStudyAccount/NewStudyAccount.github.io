97574d5ac3baccce145f4819bcf4f7a20851dd18
qjj <mr.boss.qjj@gmail.com>
2023-01-28 22:47:10 +0800

    新增

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,6 @@ cd docs/.vuepress/dist # 进入生成的文件夹
 echo 'vblog.qjjstudy.asia' > CNAME
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  # git@github.com:SaulJWu/SaulJWu.github.io.git
-#  git@github.com:NewStudyAccount/vblog.git
   githubUrl=git@github.com:NewStudyAccount/vblog.git
 else
   msg='来自github action的自动部署'
@@ -22,16 +20,16 @@ git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 
-# # deploy to coding
-# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@git.dev.tencent.com:xugaoyi/xugaoyi.git
-# else
-#   codingUrl=https ://xugaoyi:${CODING_TOKEN}@git.dev.tencent.com/xugaoyi/xugaoyi.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
+# deploy to coding
+#echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+#if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#  codingUrl=git@git.dev.tencent.com:xugaoyi/xugaoyi.git
+#else
+#  codingUrl=https://xugaoyi:${CODING_TOKEN}@git.dev.tencent.com/xugaoyi/xugaoyi.git
+#fi
+#git add -A
+#git commit -m "${msg}"
+#git push -f $codingUrl master # 推送到coding
 
 cd -
 rm -rf docs/.vuepress/dist
